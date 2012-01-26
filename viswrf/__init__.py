f4bb6509e5e4d10def054a3d3d482a801f0be873
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-01-26 18:51:41 +0900

    * viswrf/__init__.py: option parsing no luck yet

--- viswrf/__init__.py
+++ viswrf/__init__.py
@@ -7,11 +7,47 @@ __license__ = 'GPL v3 or later'
 
 __version__ = 'no version yet'
 
-import optparse
+import os
+import sys
+
+
+def parseOpts():
+    """bring Options
+    """
+    import optparse
+
+    parser = optparse.OptionParser()
+
+    # option groups
+    general = optparse.OptionGroup(parser, 'General Options')
+    #    dirconfig  = optparse.OptionGroup(parser, 'Configuration Options For Directories')
+
+    plottype = optparse.OptionGroup(parser,
+        'Plot Options')
+
+
+    general.add_option('-v', '--version',
+        action='version', help='P')
+
+    plottype.add_option("xy",
+        action='store_true', dest='plot_type', help=" Plot Type XY")
+
+
+    parser.add_option_group(general)
+    parser.add_option_group(plotxy)
+
+    opts, args = parser.parse_args(argv)
+
+    return parser, opts, args
 
 
 def main():
-    parser
+    parser, opts, args = parseOpts()
+
+    if args.length == 0:
+        print("no args")
+    else:
+        print("Good JOB")
 
 if __name__ == '__main__':
     main()
