936d1a43f6663d9128caa0ca4e35a1595c4bf199
Yakkala Yagnesh Raghava <yagnesh@live.com>
2012-04-09 00:02:19 +0900

    * viswrf/__init__.py: argparse for conf is finished

--- viswrf/__init__.py
+++ viswrf/__init__.py
@@ -14,9 +14,28 @@ def main():
     parser = argparse.ArgumentParser(
         description='Configuration and Plotting Options')
 
-    parser.add_argument('-c', '--conf', default='conf')
-    parser.add_argument('-p', '--plot', default='Test')
+    parser.add_argument('-c', '--conf',
+                        action='append',
+                        dest='confargs',
+                        nargs='+',
+                        default=[],
+                        help='Add configuration options Eg: confsetting="a"')
+
+    parser.add_argument('-p','--plot',
+                        action='append',
+                        nargs='+',
+                        default=[],
+                        help="make a plot")
+
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
+
     args = parser.parse_args()
+    # print(args)
+
+    if args.confargs:
+        c = conf.Conf(args.confargs)
 
 # ---------------------
 if __name__ == '__main__':
